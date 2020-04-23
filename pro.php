f01747c19187a14bb6fe510bb8cb13e346ab45fd
Mariongus <mariongux@gmail.com>
2020-04-23 02:31:21 -0600

    onlyphp 1.4

--- pro.php
+++ pro.php
@@ -1,13 +1,2 @@
 <!-- Version pro 1.0.1 -->
-<?php
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>FUNCA</h1>
-</body>
-</html>
+<?php include_once("pro.html"); 
